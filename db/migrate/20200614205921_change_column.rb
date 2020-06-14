2711942a6b17431a12ee69c07a0da35f99107aa5
Dennis <Dennis.portillo@yale.edu>
2020-06-14 14:00:58 -0700

    Done.

---
+++ db/migrate/20200614205921_change_column.rb
@@ -0,0 +1,6 @@
+class ChangeColumn < ActiveRecord::Migration[6.0]
+  def change
+    change_column :costume_stores, :opening_time, :datetime
+    change_column :costume_stores, :closing_time, :datetime
+  end
+end
